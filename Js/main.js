a462a7399a4fc1141d9dc27f34c6574dc55311bf
parsa.black <parsagorgzan1225@gmail.com>
2022-10-04 19:36:04 +0330

    Create Navbar and main.JS and toggle

---
+++ Js/main.js
@@ -0,0 +1,6 @@
+const toggler = document.querySelector(".nav__toggler");
+const navbar = document.querySelector(".nav");
+toggler.addEventListener("click", (e) => {
+    console.log("Clicked!");
+    navbar.classList.toggle("nav__expanded");
+});
